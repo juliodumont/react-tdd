6d1cb2bed67f94819b594e05fe4721617aed77ea
juliodumont <julioef.dumont@gmail.com>
2022-08-26 08:53:36 -0300

    Redirect test done, working on form errors

--- frontend/src/pages/Admin/Employees/Form/index.tsx
+++ frontend/src/pages/Admin/Employees/Form/index.tsx
@@ -1,9 +1,27 @@
+import history from 'util/history';
+import { useForm } from 'react-hook-form';
 import './styles.css';
+
+
+type EmployeeInfo = {
+  name: string;
+  email: string;
+}
 
 const Form = () => {
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<EmployeeInfo>();
+
   const handleCancel = () => {
-    // to do
+    history.push('/admin/employees')
+  };
+
+  const onSubmit = (EmployeeData: EmployeeInfo) => {
+    console.log(EmployeeData);
   };
 
   return (
@@ -11,25 +29,35 @@ const Form = () => {
       <div className="base-card employee-crud-form-card">
         <h1 className="employee-crud-form-title">INFORME OS DADOS</h1>
 
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="row employee-crud-inputs-container">
             <div className="col employee-crud-inputs-left-container">
 
               <div className="margin-bottom-30">
-                <input type="text" 
-                  className="form-control base-input is-invalid"
+                <input type="text"
+                  {...register('name', {
+                    required: 'Campo obrigatório',
+                  })}
+                  className={`form-control base-input ${!errors.name ? '' : 'is-invalid'}`}
                 />
                 <div className="invalid-feedback d-block">
-                  Mensagem de erro
+                  {errors.name?.message}
                 </div>
               </div>
 
               <div className="margin-bottom-30">
-                <input type="text" 
-                  className="form-control base-input"
+                <input type="text"
+                  {...register('email', {
+                    required: 'Campo obrigatório',
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: 'Email inválido'
+                    }
+                  })}
+                  className={`form-control base-input ${!errors.email ? '' : 'is-invalid'}`}
                 />
                 <div className="invalid-feedback d-block">
-                  
+                  {errors.email?.message}
                 </div>
               </div>
 
